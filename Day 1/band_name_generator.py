861065c7c39460b5f0be2969c5d78ac76e039092
JairoSolizV <146392601+JairoSolizV@users.noreply.github.com>
2025-12-01 19:55:00 -0400

    Day 01: Completed Band Name Generator

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!!!")
+city = input("For start tell me the city where you grew up\n")
+pet = input("Tell me what is the name of your pet\n")
+
+print("Your Band Name is: " + city + " " + pet)
